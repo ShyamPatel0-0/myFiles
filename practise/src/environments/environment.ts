d5ddaf5d0e68e0969a5be368d51c0125304e8c7d
ShyamPatel0-0 <shyam.mendpara01@gmail.com>
2024-06-11 16:03:46 +0530

    environment file added (stackblitz)

---
+++ practise/src/environments/environment.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: false,
+    firebaseConfig: {
+        apiKey: "AIzaSyAX8qAS8qsn4dJ0p_VcYNdnnpzsWuTssX8",
+        authDomain: "chat-4eff4.firebaseapp.com",
+        databaseURL: "https://chat-4eff4-default-rtdb.asia-southeast1.firebasedatabase.app",
+        projectId: "chat-4eff4",
+        storageBucket: "chat-4eff4.appspot.com",
+        messagingSenderId: "757975022681",
+        appId: "1:757975022681:web:78cb2a97339364c6340548",
+        measurementId: "G-DS8DG0D1WG"
+    }
+};
